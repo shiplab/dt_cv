3d492c33e80db1d5a18a1410cd4426c34d8877a8
Icaro Fonseca <icarofonseca@users.noreply.github.com>
2021-02-12 16:38:01 +0100

    Move OpenCV to libs folder

--- libs/utils.js
+++ libs/utils.js
@@ -2,7 +2,7 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
     let self = this;
     this.errorOutput = document.getElementById(errorOutputId);
 
-    const OPENCV_URL = 'opencv.js';
+    const OPENCV_URL = 'libs/opencv.js';
     this.loadOpenCv = function(onloadCallback) {
         let script = document.createElement('script');
         script.setAttribute('async', '');
